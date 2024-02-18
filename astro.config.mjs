de77dc29f86dc4dd38fa1ad2792b692303964c86
ushiromuki <ushiromukinohimitaine@gmail.com>
2024-02-18 14:25:21 +0000

    vercelアダプター追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,21 @@ import { CUSTOM_DOMAIN, BASE_PATH } from './src/server-constants';
 import CoverImageDownloader from './src/integrations/cover-image-downloader';
 import CustomIconDownloader from './src/integrations/custom-icon-downloader';
 import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
+import PageCoverImageDownloader from './src/integrations/page-cover-image-downloader';
 import PublicNotionCopier from './src/integrations/public-notion-copier';
+import vercel from '@astrojs/vercel/serverless';
 
 const getSite = function () {
   if (CUSTOM_DOMAIN) {
     return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
   }
-
   if (process.env.VERCEL && process.env.VERCEL_URL) {
     return new URL(BASE_PATH, `https://${process.env.VERCEL_URL}`).toString();
   }
-
   if (process.env.CF_PAGES) {
     if (process.env.CF_PAGES_BRANCH !== 'main') {
       return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
     }
-
     return new URL(
       BASE_PATH,
       `https://${new URL(process.env.CF_PAGES_URL).host
@@ -27,7 +26,6 @@ const getSite = function () {
         .join('.')}`
     ).toString();
   }
-
   return new URL(BASE_PATH, 'http://localhost:4321').toString();
 };
 
@@ -35,10 +33,13 @@ const getSite = function () {
 export default defineConfig({
   site: getSite(),
   base: BASE_PATH,
+  output: 'hybrid',
   integrations: [
     CoverImageDownloader(),
     CustomIconDownloader(),
     FeaturedImageDownloader(),
     PublicNotionCopier(),
+    PageCoverImageDownloader(),
   ],
+  adapter: vercel(),
 });
