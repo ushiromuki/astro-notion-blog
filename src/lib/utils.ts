aefdd793474787bd14e4f96e651b236703cdfb23
ushiro <ushiromukinohimitaine@gmail.com>
2025-03-18 13:24:13 +0000

    Add utility functions for array chunking and wait promise; migrate OG image generation to functions directory

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,15 @@ export const pathJoin = (path: string, subPath: string) => {
       .join('/')
   )
 }
+export const chunkArray = (array: [], size: number) => {
+  const result = []
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, i + size))
+  }
+  return result
+}
+
+// wait処理promiseを返す
+export const wait = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
